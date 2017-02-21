6935b79650104bf2c622305c9946d25d00a0c725
liuchanghong <liuchanghong@outlook.com>
2017-02-21 22:04:00 +0800

    Initial Commit

---
+++ PercentAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PercentAPP
+//
+//  Created by LiuChanghong on 2017/2/21.
+//  Copyright © 2017年 LiuChanghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PercentAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PercentAPP
+//
+//  Created by LiuChanghong on 2017/2/21.
+//  Copyright © 2017年 LiuChanghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
